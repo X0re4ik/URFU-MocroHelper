ecdc76582429a07fc4f8f5adfd5d0e65e51c0ba4
X0re4ik <xoore4ik@gmail.com>
2025-06-01 13:44:18 +0500

    init

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from fastapi.responses import HTMLResponse
 from src.feature.get_last_detection_service import GetLastDetectionServiceFactory
 from src.feature.get_server_info import GetServerInfoServiceFactory
 
+from src.shared.api.logger import Logger
+
+logger = Logger
+
 
 app = FastAPI()
 
@@ -13,21 +17,25 @@ get_last_detection_service = GetLastDetectionServiceFactory.create()
 get_server_info_service = GetServerInfoServiceFactory.create()
 
 
-
 @app.get("/api/v1/micro-helper/server-info")
 async def server_info_router():
     results = await get_server_info_service.get_server_info()
     return results.model_dump(by_alias=True)
 
+
 @app.websocket("/api/v1/micro-helper/last-detection")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
+        user_data = await websocket.receive_text()
+        logger.info(f"I get: {user_data}")
+
         result = await get_last_detection_service.get_last_detection()
+        
+        if result is None:
+            logger.error("No data")
+            await websocket.send_text("No data")
 
         data = result.model_dump_json(by_alias=True)
-        
-        print(data, type(data), "data")
 
         await websocket.send_text(data)
